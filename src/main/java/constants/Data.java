74d33403721cf38da818ba627e060ab6a87f7856
Anton.Karpenko <Anton.Karpenko@nexign.com>
2020-03-22 21:32:03 +0300

    Added constants

---
+++ src/main/java/constants/Data.java
@@ -0,0 +1,10 @@
+package constants;
+
+public class Data {
+    public static final String DEFAULT_EMAIL = "ak-test";
+    public static final String DEFAULT_EMAIL_SUFFIX = "@list.ru";
+    public static final String DEFAULT_PASSWORD = "apRrAVU1aa3*";
+    public static final String DEFAULT_USER_NAME = "Test*";
+    public static final String DEFAULT_USER_LAST_NAME = "AK";
+
+}
---
+++ src/main/java/constants/Routes.java
@@ -0,0 +1,6 @@
+package constants;
+
+public class Routes {
+    public static final String MAIN_PAGE = "https://mail.ru/";
+    public static final String INBOX_PAGE = "inbox/";
+}
